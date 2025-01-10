354625c85ecd78b6b17cabac83a6f8c4f2befca6
Chetan <cckamble2004@gmail.com>
2025-01-10 14:10:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,51 @@
-let speech=new SpeechSynthesisUtterance();
-
-let voices=[];
+let speech = new SpeechSynthesisUtterance();
+let voices = [];
 let voiceSelect = document.querySelector("select");
 
-window.speechSynthesis.onvoiceschanged=()=>{
-    voices=window.SpeechSynthesis.getVoices();
-    speech.voice=voices[0];
+// Populate available voices
+window.speechSynthesis.onvoiceschanged = () => {
+    voices = window.speechSynthesis.getVoices();
+    speech.voice = voices[0]; // Set default voice
 
-    voices.forEach((voice,i)=>(voiceSelect.options[i]=new Option(voice.name,i)));
+    voices.forEach((voice, index) => {
+        voiceSelect.options[index] = new Option(voice.name, index);
+    });
 };
-voiceSelect
-.addEventListener("change",()=>{
-    speech.voice=voices[voiceSelect.value];
 
+voiceSelect.addEventListener("change", () => {
+    speech.voice = voices[voiceSelect.value];
 });
 
-document.querySelector("button").addEventListener("click",()=>{
-    speech.text =document.querySelector("textarea").value;
-    window.speechSynthesis
-    .speak(speech);
-})
+document.querySelector("#speakButton").addEventListener("click", () => {
+    let textInput = document.querySelector("textarea").value;
+    if (textInput.trim() === "") {
+        alert("Please enter some text before speaking!");
+        return;
+    }
+    
+    speech.text = textInput;
+
+    // Start speech synthesis
+    window.speechSynthesis.speak(speech);
+
+    // Record and prepare audio for download
+    let audioContext = new AudioContext();
+    let destination = audioContext.createMediaStreamDestination();
+    let mediaRecorder = new MediaRecorder(destination.stream);
+
+    let audioChunks = [];
+    speech.onstart = () => mediaRecorder.start();
+    speech.onend = () => mediaRecorder.stop();
+
+    mediaRecorder.ondataavailable = event => audioChunks.push(event.data);
+
+    mediaRecorder.onstop = () => {
+        const audioBlob = new Blob(audioChunks, { type: 'audio/mp3' });
+        const audioURL = URL.createObjectURL(audioBlob);
+
+        const downloadButton = document.querySelector("#downloadButton");
+        downloadButton.href = audioURL;
+        downloadButton.download = "speech.mp3";
+        downloadButton.style.display = "inline-block"; // Show button
+    };
+});
